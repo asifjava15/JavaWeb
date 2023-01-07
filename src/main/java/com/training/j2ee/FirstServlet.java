cc8a37914b055933ffb647bbd732471a2b8b2b0f
asifjava15 <asif_java@outlook.com>
2023-01-06 19:27:47 -0500

    6Jan2023

---
+++ src/main/java/com/training/j2ee/FirstServlet.java
@@ -0,0 +1,34 @@
+package com.training.j2ee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("<h1>First Servlet Program executed well</h1>");
+		String name=request.getParameter("name");
+		out.println("<h1>Name is:"+name+"</h1>");
+	}
+
+}
